58a09909e81621738fd97506658d3836a7c7d809
Tamara Palmer <palmert974@macomb.com>
2023-04-03 23:23:58 -0400

    updated folder structure,added partials,Main and Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../../assets/Logo/BrainFlix-logo.svg";
+import profilePicture from "../../ assets/Images/Mohan-muruge.jpg";
+import "./Header.scss";
+
+const Header = () => (
+  <header className="header">
+    <Brand />
+    <Search />
+    <div className="header__upload">
+      <UploadButton />
+      <ProfilePicture />
+    </div>
+  </header>
+);
+
+const Brand = () => (
+  <div className="header__image">
+    <img src={logo} alt="Brand-Logo" className="header__logo" />
+  </div>
+);
+
+const Search = () => (
+  <form className="header__form">
+    <input
+      type="search"
+      placeholder="Search"
+      className="header__search"
+    ></input>
+  </form>
+);
+
+const UploadButton = () => (
+  <button className="header__button button">+ UPLOAD</button>
+);
+
+const ProfilePicture = () => (
+  <img
+    src={profilePicture}
+    alt="Profile-Picture"
+    className="header__profile-picture"
+  />
+);
+
+export default Header;
