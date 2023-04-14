c5d40e98ddbf4b6129c0cd2710fb89469331bc26
Tamara Palmer <palmert974@macomb.com>
2023-04-14 18:01:34 -0400

    updated Main.js started axious grabbed data, and uplaod video

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import videos from "../../data/videos.json";
 import videoDetails from "../../data/video-details.json";
 import VideoDetails from "../../components/VideoDetails/VideoDetails";
@@ -9,6 +11,31 @@ import "./Main.scss";
 
 function Main() {
   const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+  const [videosData, setVideosData] = useState([]);
+  const { idFromParams } = useParams();
+
+  const apiKey = "?api_key=9dc66532-85bb-4503-97b9-99f84eeabec3";
+  const baseUrl = "https://project-2-api.herokuapp.com/videos";
+
+  let defaultPlantId = null;
+
+  if (videosData.length > 0) {
+    defaultPlantId = videosData[0].id;
+
+    console.log(defaultPlantId);
+  }
+
+  // `||` evaluates to the first expression if it exists, or it uses the second
+  let plantIdToDisplay = idFromParams || defaultPlantId;
+
+  useEffect(() => {
+    axios.get(baseUrl + apiKey).then((response) => {
+      console.log(response.data);
+      setVideosData(response.data);
+    });
+  }, []);
+
+  // const filteredPlants = plants.filter(plant => plant.id !== plantIdToDisplay)
 
   // create plantClick function - takes plant id
   // Function to call when a nav button is clicked
@@ -21,9 +48,10 @@ function Main() {
 
   // create filteredPlants by filtering the current selected plant out of the array
   // filteredPlants is passed to Nav
-  const filteredVideos = videos.filter(
-    (video) => video.id !== selectedVideo.id
+  const filteredVideos = videosData.filter(
+    (video) => video.id !== defaultPlantId.id
   );
+  console.log(filteredVideos);
 
   return (
     <div className="main">
--- src/pages/UploadVideo/UploadVideo.js
+++ src/pages/UploadVideo/UploadVideo.js
@@ -1,7 +1,60 @@
-function UploadVideo () {
-    return (
-        <h1>Hello Fyiah</h1>
-    )
+import "./UploadVideo.scss";
+import videoThumbnail from "../../assets/Images/logo/Upload-video-preview.jpg";
+
+function UploadVideo() {
+  return (
+    <>
+      <hr className="upload__divider"></hr>
+      <div className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+        <form>
+          <div className="upload__videocontainer">
+            <label htmlFor="video" className="upload__label">
+              VIDEO THUMBNAIL
+            </label>
+            <img
+              className="upload__video"
+              src={videoThumbnail}
+              alt="Video-Thumbnail"
+              name="video"
+            ></img>
+          </div>
+
+          <div className="upload__textcontainer">
+            <label htmlFor="videoTitle" className="upload__label">
+              TITLE YOUR VIDEO
+            </label>
+            <input
+              className="upload__videotitle"
+              type="text"
+              name="videoTitle"
+              placeholder="Add a title to your video"
+            ></input>
+
+            <label htmlFor="comment" className="upload__label">
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <textarea
+              className="upload__textarea"
+              name="comment"
+              cols="30"
+              rows="5"
+              placeholder="Add a description of your video"
+            ></textarea>
+          </div>
+
+          <div className="upload__buttons">
+            <button type="submit" className="upload__submit">
+              PUBLISH
+            </button>
+            <button type="submit" className="upload__cancel">
+              CANCEL
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default UploadVideo
+export default UploadVideo;
